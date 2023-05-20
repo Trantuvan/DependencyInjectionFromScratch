0cb0c2adbc72689ff1923e418c5e69eda2a2bf6b
trantuvan <trantuvan.kan@gmail.com>
2023-05-20 16:25:39 +0700

    DiContainer
    This class provides a Di container which acts as a scope for service requests.

---
+++ DependencyInjectionFromScratch/DependencyInjection/DiContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace DependencyInjectionFromScratch.DependencyInjection;
+
+public class DiContainer
+{
+    public T GetService<T>()
+    {
+        throw new NotImplementedException();
+    }
+}
