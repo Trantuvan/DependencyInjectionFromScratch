9de9ebcc990edb1bee050766fd25325071ee737e
trantuvan <trantuvan.kan@gmail.com>
2023-05-20 20:54:56 +0700

    GetService for generic Register
    Register concrete vs Register generic
    + Register generic just have serviceType as compile time
    + GetService must new up implementation (use SystemActivator)
    
    GetService Singleton vs GetService Transient
    + Transient new up implemation everytime GetSerive call (use
      System.Activator)
    + Singleton must return reference from 2nd call to GetService (override
      descriptor.Implementation & return reference from check desc.Impl is
      not null from 2nd call from GetService)

--- DependencyInjectionFromScratch/DependencyInjection/DiContainer.cs
+++ DependencyInjectionFromScratch/DependencyInjection/DiContainer.cs
@@ -18,12 +18,22 @@ public class DiContainer
             throw new Exception($"Service of type {typeof(T).Name} isn't register");
         }
 
-        if (descriptor.Implementation is null)
+        // for concrete impl type
+        if (descriptor.Implementation is not null)
         {
-            return default!;
+            return (T)descriptor.Implementation;
         }
 
-        return (T)descriptor.Implementation;
-    }
+        // for generic register | use System.Activator to create new instance
+        // for impl Type
+        var implementation = (T)Activator.CreateInstance(descriptor.ServiceType);
 
+        // Singleton overload Impl to return impl reference
+        if (descriptor.Lifetime.Equals(ServiceLifetime.Singleton))
+        {
+            descriptor.Implementation = implementation;
+        }
+
+        return implementation;
+    }
 }
