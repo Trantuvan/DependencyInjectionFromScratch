e996ce0c83ff555a58efc090c3e4b3fee71b2936
trantuvan <trantuvan.kan@gmail.com>
2023-05-20 17:49:20 +0700

    GetService
    Return service impl if ServiceType == generic type | cast the impl to
    generic type
    
    Check null:
    + Service of type Descriptor null -> throw Execption
    + Service impl null -> return default value

--- DependencyInjectionFromScratch/DependencyInjection/DiContainer.cs
+++ DependencyInjectionFromScratch/DependencyInjection/DiContainer.cs
@@ -3,8 +3,27 @@
 public class DiContainer
 {
     private readonly List<ServiceDescriptor> _serviceDescriptors;
+
+    public DiContainer(List<ServiceDescriptor> serviceDescriptors)
+    {
+        _serviceDescriptors = serviceDescriptors;
+    }
     public T GetService<T>()
     {
-        throw new NotImplementedException();
+        var descriptor = _serviceDescriptors
+                   .FirstOrDefault(service => service.ServiceType == typeof(T));
+
+        if (descriptor is null)
+        {
+            throw new Exception($"Service of type {typeof(T).Name} isn't register");
+        }
+
+        if (descriptor.Implementation is null)
+        {
+            return default!;
+        }
+
+        return (T)descriptor.Implementation;
     }
+
 }
