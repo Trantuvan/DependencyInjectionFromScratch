51042910def98692287e760ecc209bd2de480d35
trantuvan <trantuvan.kan@gmail.com>
2023-05-20 16:28:45 +0700

    DiServiceCollection
    This class is the collection which holds services.
    
    + RegisterSingleton method returns a way to add service to collection (singleton lifetime)
    + GenerateContainer method returns the DiContainer (the container scope).

---
+++ DependencyInjectionFromScratch/DependencyInjection/DiServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjectionFromScratch.DependencyInjection;
+
+public class DiServiceCollection
+{
+    public void RegisterSingleton<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    public DiContainer GenerateContainer()
+    {
+        throw new NotImplementedException();
+    }
+}
